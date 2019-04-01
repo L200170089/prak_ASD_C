62b7697b3c695c04c3bff45fcfcc8ee511d87dad
L200170089 <48180110+L200170089@users.noreply.github.com>
2019-04-02 01:46:12 +0700

    Add files via upload

---
+++ modul 4/nomer 6-7.py
@@ -0,0 +1,36 @@
+print (" Praktikum Modul 4","\n",
+       "Nama     :Xenyx S","\n",
+"NIM/Kelas:L200170089/C","\n","\n")
+
+
+A = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]
+
+#Nomer 6
+def binSe(target):
+    low = 0
+    high = len(A)
+
+    while low < high:
+        mid = (high + low) // 2
+        if A[mid] == target:
+            return "Target pada indeks " + str(mid)
+        elif target < A[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return False
+
+#Nomer 7
+B = [2, 3, 5, 6, 6, 6, 8, 9, 9, 10, 11, 12, 13, 13, 14]
+def binSe2(target):
+    low = 0
+    high = len(B)
+    x = []
+
+    while low < high:
+        if B[low] == target:
+            x.append(low)
+            low+=1
+        else:
+            low+=1
+    return x
